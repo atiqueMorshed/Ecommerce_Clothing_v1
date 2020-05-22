adc91dc209488c2d37756ba8458ff63dcb1bf7b9
ArrowX17 <iamsamix17@gmail.com>
2020-05-23 03:01:10 +0600

    Signin form +FormInput +CustomButton components

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, ...otherFormInputProps }) => (
+  <div className="group">
+    <input
+      className="form-input"
+      onChange={handleChange}
+      {...otherFormInputProps}
+    ></input>
+    {label ? (
+      <label
+        className={`${
+          otherFormInputProps.value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+export default FormInput;
